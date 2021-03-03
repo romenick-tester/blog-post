8a948b12d74572649cce87ef86bb8e9d28671edd
Romenick Tester <romenick.tester@gmail.com>
2021-03-03 15:59:48 +0000

    compose view and delete posts | completed

---
+++ README.md
@@ -0,0 +1,5 @@
+# Daily Journal
+>A simple social web app that lets you blogs...
+
+### requirements:
+> nodejs, express, ejs
--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 
 const [home, about, contact] = require("./content");
 
-const newBlogs = [];
+let newBlogs = [];
 
 app.get("/", (req,res) => {
     res.render("home", {title: "Home", body: home});
@@ -24,23 +24,53 @@ app.get("/contact", (req,res) => {
 });
 
 app.get("/blog", (req,res) => {
-    res.render("post", {blogs: newBlogs});
+    res.render("post", { blogs: newBlogs });
 });
 
 app.get("/compose", (req,res) => {
     res.render("compose");
 });
 
-app.post("/compose", (req,res) => {
+app.post("/compose", (req, res) => {
     const newPost = {
+        id: newBlogs.length + 1,
         title: req.body.title,
         body: req.body.body
     }
 
     newBlogs.push(newPost);
 
-    res.redirect("/");
+    res.redirect("/blog");
 });
+
+app.get("/posts/:id", (req, res) => {
+    const postId = req.params.id;
+
+    const query = newBlogs.find((item) => item.id === Number(postId));
+
+    if (!query) {
+
+        res.send("this post is not recorded...");
+
+    } else {
+
+        const { title, body, id } = query;
+
+        res.render("singlePost", { title, body, id });
+    }
+})
+
+app.get("/post/:id", (req, res) => {
+    const postId = req.params.id;
+
+    const updatedBlogs = newBlogs.filter((item) => item.id !== Number(postId));
+
+    newBlogs = updatedBlogs;
+
+    res.redirect("/blog");
+})
+
+
 
 let port = process.env.PORT || 5000;
 
